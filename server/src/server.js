bbc78d9bf552787bd55d382c20d0dc1a01b3623a
TanveerS24 <stanveer1809@gmail.com>
2025-08-20 02:05:54 +0530

    Initial Commmit

---
+++ server/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import connectDB from "./config/db.js";
+import expenseRouter from "./routes/expense.js";
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/expenses", expenseRouter);
+app.use("/api/users", userRouter);
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+});
+
+
