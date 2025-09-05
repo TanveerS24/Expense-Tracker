94a1e9bf0b03bef7bdffe228f3445e663d897c33
TanveerS24 <stanveer1809@gmail.com>
2025-09-05 12:24:02 +0530

    created log out button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,20 @@ import Dock from "../react-bits/Dock";
 import Home from '../icons/Home.png'
 import Add from '../icons/Add.png'
 import User from '../icons/User.png'
+import Logout from '../icons/Logout.png'
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const logout = () => {
+    sessionStorage.clear();
+    navigate("/");
+  }
   const items = [
     { icon: <img src={Home} alt="Home" />, label: "Home", onClick: () => navigate("/home") },
     { icon: <img src={Add} alt="Add record" />, label: "Add Record", onClick: () => navigate("/newRecord") },
     { icon: <img src={User} alt="Profile" />, label: "Profile", onClick: () => navigate("/userdetails") },
+    { icon: <img src={Logout} alt="Logout" />, label: "Logout", onClick: logout },
   ];
 
   return (
--- client/src/components/RecordCard.jsx
+++ client/src/components/RecordCard.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const RecordCard = ({ record }) => {
   return (
     <div>
-      <h2>{record.title}</h2>
       <p>{record.description}</p>
       <p>{record.amount}</p>
+      <p>{record.date}</p>
+      <p>{record.category}</p>
+      <p>{record.transactionType}</p>
     </div>
   )
 }
